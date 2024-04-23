b93976bd12eef6835406f20568504a24c91130b5
martinamicaela frutos <martinamicaela924@gmail.com>
2024-04-23 21:39:52 +0000

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,15 @@ let firstResult=null;
 let secondResult=null;
 let movements=0;
 let rightAnswers=0;
+let timer=false;
+let timerTotal=30;
+let regressiveTime=null;
 
 
 // pointing document HTML 
 let showMovements=document.getElementById('movements');
-let showRightAnswers=document.getElementById('right-answers')
+let showRightAnswers=document.getElementById('right-answers');
+let showTime=document.getElementById('timeLeft');
 
 
 //random numbers generation 
@@ -18,8 +22,24 @@ let numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 numbers= numbers.sort(()=>{return Math.random()-0.5});
 console.log(numbers);
 
+//functions 
+function countTime(){
+  regressiveTime= setInterval(() => {
+    timerTotal--;
+    showTime.innerHTML=`Time: ${timerTotal} seconds`
+    if(timerTotal==0){
+      clearInterval(regressiveTime);
+      
+    }
+  }, 1000);
+};
+
 //main function
 function uncover(id){
+  if(timer==false){
+    countTime();
+    timer=true;
+  }
     uncoveredCards++;
     console.log(uncoveredCards)
   if(uncoveredCards==1){
